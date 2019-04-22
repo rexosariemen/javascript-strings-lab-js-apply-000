3054b3bcd5b7a2e98b010f934db600cd71a27cfd
Rex Osariemen <rexosariemen@yahoo.com>
2019-04-22 23:52:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var specialGuest = "Neil deGrasse Tyson"
 var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "Let's talk about space.";
-//There is an error with the test for the var topic.
+//There is an error with the tests for the var topic.
 //my input into the 'var topic', //=> "Let's talk about space."
 //It keep repeating itself in the ouptut more than once.
 //The expected keep changing.
